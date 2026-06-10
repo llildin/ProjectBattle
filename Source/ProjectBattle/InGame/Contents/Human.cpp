47387817a4b4e44415f77b31cfe459fe2d1875eb
llildin <132454924+llildin@users.noreply.github.com>
2026-06-10 13:42:35 +0900

    연습용 허수아비 생성

---
+++ Source/ProjectBattle/InGame/Contents/Human.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InGame/Contents/Human.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+AHuman::AHuman()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Katana = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Katana"));
+	Katana->SetupAttachment(GetMesh(), TEXT("Katana_Socket"));
+
+	GetMesh()->SetRelativeLocationAndRotation(
+		FVector(0, 0, -GetCapsuleComponent()->GetScaledCapsuleHalfHeight()),
+		FRotator(0, -90.f, 0)
+	);
+}
+
+// Called when the game starts or when spawned
+void AHuman::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHuman::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHuman::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
