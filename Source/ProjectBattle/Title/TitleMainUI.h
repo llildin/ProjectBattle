f765a20df765144534da33844433121f26263999
llildin <132454924+llildin@users.noreply.github.com>
2026-06-23 14:26:23 +0900

    UI 기반 제작

---
+++ Source/ProjectBattle/Title/TitleMainUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleMainUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBATTLE_API UTitleMainUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
